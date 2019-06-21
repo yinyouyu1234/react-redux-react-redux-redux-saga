34299e3ed359b21fc315a3d1308ee604f98f36c1
git <yin_you_yu@163.com>
2019-06-21 11:54:31 +0800

    a

--- src/a.js
+++ src/a.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import About from './page/about/About'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom'
+
 
 export default class A extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+
+    console.log(this.props)
+
+  }
 
   render() {
     console.log(111111)
     const auth = Math.random() < 0.5
     console.log(auth, Math.random)
-    return (
-      auth && <Route path='/about' exact component={About} />
-    )
+    if (auth) {
+      return (
+        <Route path='/about' exact component={About} />
+      )
+    } else {
+      return (
+        <Redirect to="/" />
+      )
+    }
+
   }
 }
